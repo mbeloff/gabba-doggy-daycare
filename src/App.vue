dd132e131e5c7ef5a1c3b1e66997574005458feb
michael beloff <michael.a.beloff@gmail.com>
2020-10-27 16:00:53 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="nav-wrapper fixed w-full" >
+      <Nav class="mb-6 flex-1" />
+    </div>
+    <div class="content pt-20 p-6 bg-gray-100">
+      <HelloWorld msg="HelloWorld Component MSG" class="text-center text-lg p-6"/>
+      <p>content</p>
+    </div>
+    <div class="footer bg-gray-800 p-6 text-gray-500">
+      <p>footer content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Nav
+  }
+}
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+body {
+  font-family: 'Roboto', sans-serif;
+}
+
+#app {
+  display: grid;
+  grid-template-rows: 1fr auto;
+  min-height: 100vh;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,72 @@
+<template>
+  <nav class="flex items-center justify-between flex-wrap p-6 shadow-lg bg-white">
+    <div class="flex items-center flex-shrink-0 text-blue-500 mr-6">
+      <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+      <span class="font-semibold text-xl tracking-tight">Gabba Doggy Daycare</span>
+    </div>
+    <div class="block lg:hidden">
+      <button v-on:click="show = !show" class="flex items-center px-3 py-2 border rounded text-blue-600 border-blue-400 hover:text-blue-400 hover:border-blue-400">
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </button>
+    </div>
+    <transition name="slide">
+      <div v-show="show" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow">
+        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-gray-700 hover:text-blue-600 mr-4">
+          Home
+        </a>
+        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-gray-700 hover:text-blue-600 mr-4">
+          Menu#2
+        </a>
+        <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-gray-700 hover:text-blue-600">
+          Menu#3
+        </a>
+      </div>
+      <div>
+        <a href="#" class="inline-block text-md px-6 py-2 leading-none rounded bg-blue-500 text-white hover:border-transparent hover:text-white hover:bg-blue-700 mt-4 lg:mt-0">Register</a>
+      </div>
+    </div>
+    </transition>
+    
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data() {
+    return {
+      show: true
+    }
+  },
+  methods: {
+  onResize() {
+    if (window.innerWidth > 1023) {
+      this.show = true
+    }
+  }
+},
+
+created() {
+  window.addEventListener('resize', this.onResize)
+  if (window.innerWidth < 1023) {
+    this.show = false 
+  }
+},
+BeforeDestroy() {
+  window.removeEventListener('resize', this.onResize)
+},
+}
+</script>
+<style>
+/* .slide-leave-active,
+.slide-enter-active {
+  transition: 1s;
+}
+.slide-enter {
+  transform: translate(-100%, 0);
+}
+.slide-leave-to {
+  transform: translate(-100%, 0);
+} */
+</style>
