a5c057847fb9a00eeaef704afe4805fd31341932
michael beloff <michael.a.beloff@gmail.com>
2020-11-05 08:11:13 +1000

    frre>trial

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,7 +3,7 @@
     <div class="container p-4 flex flex-col justify-center h-full mx-auto">
       <div class="xs:w-full sm:w-2/3 md:w-1/2 lg:w-1/3">
         <p class="text-6xl font-bold leading-tight tracking-tight mb-3">A great day out for your pup</p>
-        <p class="mb-5 tracking-wider text-lg"><span class="font-bold text-orange-500">OPENING SOON!</span> Register your details early for <nobr>3-days</nobr> free at Gabba Doggy Day Care.</p>
+        <p class="mb-5 tracking-wider text-lg"><span class="font-bold text-orange-500">OPENING SOON!</span> Register your details early for <nobr>3-day</nobr> trial at Gabba Doggy Day Care.</p>
         <open-modal class="border text-lg rounded-full px-5 py-2 hover:bg-white hover:text-black focus:outline-white"><i class="far fa-comment-alt-lines fa-fw mr-2"></i>Register</open-modal>
       </div>
     </div>  
