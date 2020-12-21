fd43722f86b1c34cbb0210182700c9283696cce4
michael beloff <michael.a.beloff@gmail.com>
2020-12-21 10:02:55 +1000

    add map and location directions

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="section">
-    <div class="container py-20 mx-auto">
+    <div class="container py-10 mx-auto">
       <Title class="text-center">
         <template v-slot:small>Get in Touch</template>
         <template v-slot:big>Contact Us</template>
       </Title>
       <div class="grid grid-cols-1 px-4 md:grid-cols-2">
-        <div class="flex-grow ml-auto mr-auto bg-no-repeat bg-contain md:ml-auto md:mr-0 phone-pup" style="height: 350px; width: 350px">
+        <div class="flex-grow ml-auto mr-auto bg-no-repeat bg-contain md:ml-auto md:mr-0 phone-pup max-w-full" style="height: 350px; width: 350px">
         </div>
         <div class="my-auto">
           <ul class="text-xl font-bold list-none list-inside">
-            <ListItem class="flex flex-col items-center mb-4 text-center md:items-start md:flex-row md:text-left">Address: <br> <span>Gabba Doggy Daycare <br> 46 Deshon Street, <br>Woolloongabba 4102</span></ListItem>
+            <ListItem class="flex flex-col items-center mb-4 text-center md:items-start md:flex-row md:text-left">Address: <br> <span>Gabba Doggy Daycare <br> 46 Deshon Street, <br>Woolloongabba 4102</span> <br> </ListItem>
+            <li class="flex flex-col items-center mb-4 text-center md:items-start md:flex-row md:text-left "><span class=" bg-blue-100 text-blue-800 font-normal py-1 px-4 text-sm">First entrance to the right as you come in through the main gate</span></li>
             <ListItem class="flex flex-col items-center mb-4 text-center md:flex-row md:text-left md:items-start">Phone: <br> <a class="font-normal link-pink" href="tel:0423487302">0423 487 302</a></ListItem>
             <ListItem class="flex flex-col items-center mb-4 text-center md:flex-row md:text-left md:items-start">Email: <br> <span><Email class="break-all">woof@gabbadoggydaycare.com</Email></span></ListItem>
             <ListItem class="flex flex-col items-center mb-4 text-center md:flex-row md:text-left md:items-start">Message: <br> <span><open-modal class="link-pink">leave a message</open-modal></span>  
@@ -28,6 +29,11 @@
           </ul>
         </div>
         
+      </div>
+    </div>
+    <div class="container mx-auto lg:pb-10">
+      <div class="xl:w-2/3 lg:w-1/2 w-full mx-auto">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14157.078780074915!2d153.0441013!3d-27.4919868!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2cbf7d0ff474ef0e!2sGabba%20Doggy%20Daycare!5e0!3m2!1sen!2sau!4v1608508120256!5m2!1sen!2sau" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
       </div>
     </div>
   </div>
