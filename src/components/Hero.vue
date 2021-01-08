b35907f125cb36ab80e0f2c13c495370ef0f3a03
michael beloff <michael.a.beloff@gmail.com>
2021-01-08 11:12:50 +1000

    copy edit

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,8 +3,7 @@
   <div class="hero-grid grid grid-cols-1 lg:grid-cols-4 lg:grid-rows-1 bg-gray-200">
     <div class="grid p-10 lg:py-40 sm:px-40 2xl:px-60 order-2 lg:order-1 col-span-2">
       <div class="place-self-center space-y-3">
-        <img class="mb-2" src="https://res.cloudinary.com/dg5ybbkbh/image/upload/c_fit,w_600/q_auto,f_auto/v1609988041/gddc/3daytrial.png" alt="">
-        <!-- <p class="text-xl md:text-2xl"><span class="font-bold text-yellow-500 uppercase">3 Days Free</span><br> New accounts get their first 3 days of daycare completely free.</p> -->
+        <img class="mb-2" src="https://res.cloudinary.com/dg5ybbkbh/image/upload/c_fit,w_600/q_auto,f_auto/v1/gddc/3daytrial.png" alt="">
         <span class="text-sm tracking-tight leading-">Free trial for dogs over 6 months only</span>
        <router-link to="Sign-up" class="btn-blue text-xl w-full block text-center">Sign me up!</router-link>
       </div>
