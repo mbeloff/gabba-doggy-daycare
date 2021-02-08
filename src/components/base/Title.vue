6be7cc61221ab9a83f4d9036591694227edb3777
michael beloff <michael.a.beloff@gmail.com>
2021-02-08 15:30:28 +1000

    adjust title spacing

--- src/components/base/Title.vue
+++ src/components/base/Title.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p class="mb-10 font-bold text-blue-500">
+    <p class="mb-5 font-bold text-blue-500">
       <slot name="small"></slot>
       </p>
       <h2 class="mb-10 text-3xl sm:text-5xl font-bold leading-tight tracking-tight"><slot name="big">Default Big</slot></h2>
