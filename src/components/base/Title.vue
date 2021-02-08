0513712b8f273e5e52d62e6047f839ce6930f06f
michael beloff <michael.a.beloff@gmail.com>
2021-02-08 15:22:25 +1000

    add responsive font sizing

--- src/components/base/Title.vue
+++ src/components/base/Title.vue
@@ -3,7 +3,7 @@
     <p class="mb-10 font-bold text-blue-500">
       <slot name="small"></slot>
       </p>
-      <h2 class="mb-10 text-5xl font-bold leading-tight tracking-tight"><slot name="big">Default Big</slot></h2>
+      <h2 class="mb-10 text-3xl sm:text-5xl font-bold leading-tight tracking-tight"><slot name="big">Default Big</slot></h2>
   </div>
 </template>
 
