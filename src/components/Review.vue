87e89aa2c343992e5936b84ebe4a49eb168c5f3e
michael beloff <michael.a.beloff@gmail.com>
2021-03-16 09:00:58 +1000

    add fb reviews

--- src/components/Review.vue
+++ src/components/Review.vue
@@ -2,11 +2,11 @@
   <div class="flex flex-col justify-between">
     <div class="m-2 px-4 py-3 bg-white rounded-lg shadow-lg border-gray-100 border" @click="expanded = !expanded">
       <div class="flex justify-between">
-        <p class="font-bold text-sm text-blue-900">{{reviewData.name}}</p><span class="text-lg"><i class="fab fa-google text-white rounded-full bg-blue-500 p-1"></i></span>
+        <p class="font-bold text-sm text-blue-900">{{reviewData.name}}</p><i :class="reviewData.icon" class="fab text-blue-500"></i>
       </div>
 
       <p ref="reviewBox" class="text-sm relative review-text" :class="{'truncate': !expanded}">{{reviewData.review}}  </p>
-      <p class="text-xs text-right text-yellow-400 mt-2">
+      <p v-if="reviewData.stars" class="text-xs text-right text-yellow-400 mt-2">
       <i class="fas fa-star" v-for="star in Math.floor(reviewData.stars)" :key="star"></i>
       <i class="fad fa-star-half" v-if="reviewData.stars % 1"></i>
     </p>
--- src/components/SectionReviews.vue
+++ src/components/SectionReviews.vue
@@ -24,31 +24,37 @@
         reviews: [{
             name: 'Hannah Roberts',
             review: 'Wonderful place! Our lab and golden retriever have been coming weekly and always have the best time. The staff are lovely and it\'s a fun environment. Couldn\'t recommend more',
-            stars: 5
-          }
-
-          ,
+            stars: 5,
+            icon: 'fa-google'
+          },
           {
             name: 'Catherine Lever',
             review: 'Lexi loves her day here, awesome staff and always adding new additions for the dogs to play',
             stars: 5,
-          }
-
-          ,
+            icon: 'fa-google'
+          },
           {
             name: 'Jennie S',
             review: 'I took advantage of the new business promotion on the 3 free days to see whether the doggy daycare would be a fit for my 2 small dogs. \nThey had to much fun on the days they were there and we will definitely be booking in again. \nThe ladies there were very professional and were very quick to answer any questions that i had.',
             stars: 5,
-          }
-
-          ,
+            icon: 'fa-google'
+          },
           {
             name: 'Kaitlyn Fogarty',
             review: '',
             stars: 5,
+            icon: 'fa-google'
+          },
+          {
+            name: 'Josephine Potts',
+            review: 'My Pippa had her first and great day at Gabba Doggy Daycare on Friday 26 February 2021. The girls looked after her and being PIPPA’s first day, they kept me up to date with how she was enjoying herself. Pippa came home exhausted and will be ready to enjoy her next visit. Thank you to everyone and the Pet Taxi was so helpful in carrying Pippa to and from. 💜🐶💜',
+            icon: 'fa-facebook-f'
+          },
+          {
+            name: 'Meika Ruby',
+            review: 'My doggy had a great time. I loved the photos they sent and could tell he was entertained all day. The staff were lovely and I could tell Billy was comfortable with them 💙',
+            icon: 'fa-facebook-f'
           }
-
-          ,
         ]
       }
     }
