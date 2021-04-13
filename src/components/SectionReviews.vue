f64cd543b6bbfe16f050dea4b067f4eaf992c598
michael beloff <michael.a.beloff@gmail.com>
2021-04-13 10:19:26 +1000

    remove full names

--- src/components/SectionReviews.vue
+++ src/components/SectionReviews.vue
@@ -22,13 +22,13 @@
     data() {
       return {
         reviews: [{
-            name: 'Hannah Roberts',
+            name: 'Hannah R',
             review: 'Wonderful place! Our lab and golden retriever have been coming weekly and always have the best time. The staff are lovely and it\'s a fun environment. Couldn\'t recommend more',
             stars: 5,
             icon: 'fa-google'
           },
           {
-            name: 'Catherine Lever',
+            name: 'Catherine L',
             review: 'Lexi loves her day here, awesome staff and always adding new additions for the dogs to play',
             stars: 5,
             icon: 'fa-google'
@@ -46,18 +46,18 @@
             icon: 'fa-google'
           },
           {
-            name: 'Elizabeth Bodis',
+            name: 'Elizabeth B',
             review: 'Great daycare! It’s always daunting leaving your fur baby somewhere, but having the 3 days free really helps you get comfortable and make sure it’s the right fit!! It definitely is, our little man loves coming here and he starts getting so excited when we open the door. Best thing is you get updates throughout the day too 😊',
             stars: 5,
             icon: 'fa-google'
           },
           {
-            name: 'Josephine Potts',
+            name: 'Josephine P',
             review: 'My Pippa had her first and great day at Gabba Doggy Daycare on Friday 26 February 2021. The girls looked after her and being PIPPA’s first day, they kept me up to date with how she was enjoying herself. Pippa came home exhausted and will be ready to enjoy her next visit. Thank you to everyone and the Pet Taxi was so helpful in carrying Pippa to and from. 💜🐶💜',
             icon: 'fa-facebook-f'
           },
           {
-            name: 'Meika Ruby',
+            name: 'Meika R',
             review: 'My doggy had a great time. I loved the photos they sent and could tell he was entertained all day. The staff were lovely and I could tell Billy was comfortable with them 💙',
             icon: 'fa-facebook-f'
           }
