39500602d44fdb4fe3e90406dbdc8b6631acb3a9
michael beloff <michael.a.beloff@gmail.com>
2021-05-12 14:48:21 +1000

    copy edit

--- src/components/SectionWhatToExpect.vue
+++ src/components/SectionWhatToExpect.vue
@@ -17,10 +17,10 @@
           We have a Pet Taxi service available for local pickup and drop-off. Please confirm availability and book in advance via <link-email class="link-pink">email.</link-email>
         </li>
         <li>
-          If your pup usually eats lunch and you would like us to feed them, we charge a $5 fee (your dog will have it's own space and supervision during feeding time). Please provide your dog's usual food, labelled with their name on it.
+          If your pup usually eats lunch and you would like us to feed them, we charge a $5 feeding fee. Please provide your dog's usual food, labeled with their name on it and pay at drop off.
         </li>
         <li>
-          Let us know at drop off if your pup is allergic to any foods as we do give treats during enrichment time.
+          Let us know at drop off if your pup is allergic to any foods as we may give treats during enrichment time.
         </li>
         <li>
           If you have any special requests for the day please let us know and our staff will do our best to accommodate.
@@ -29,7 +29,7 @@
           If your dog is sick, please refrain from booking them into daycare until they're in good health and have vet clearance to attend.
         </li>
       </ul>
-      
+
     </div>
     <div class="text-sm mb-3">
       <h2 class="font-bold text-xl mb-3">During Daycare</h2>
@@ -49,41 +49,46 @@
         <li>
           <span class="font-bold">Photos/Videos -</span> We do our best to keep you updated through the day! Our main focus however, is on giving our pups the best possible experience. To keep updated through the day, head to our <a target="_blank" rel="noopener" href="https://www.instagram.com/gabbadoggydaycare/" aria-label="Find us on Instagram" title="Find us on Instagram" class="link-pink">Instagram</a> stories, where we post all the fun we're having through the day.
         </li>
-        
+
 
       </ul>
-      
+
     </div>
     <div class="text-sm mb-3">
       <h2 class="font-bold text-xl mb-3">After Daycare</h2>
-      <ul class="list-inside list-none">        
+      <ul class="list-inside list-none">
         <li>
           <span class="font-bold">Boo boos/scratches -</span> We do our absolute best to monitor all dogs throughout the day however as dogs are animals, things can happen quickly. If we notice any bite, cut or scratch on your dog we will inform you right away, however, while we do provide the utmost care and attention to our doggy guests, accidents will sometimes happen and not everything can be identified.
         </li>
+        
         <li>
-          <span class="font-bold">Picking Up -</span> Please note that while we close at {{ this.closeTime }}, we ask that you aim to arrive earlier than this if possible, to account for any delays (e.g. traffic) and prevent a rush at closing time. If you are running late, please let us know as early as possible.
+          <span class="font-bold">Hungry or Tired -</span> As your dog is likely to be much more active at daycare than at home, you may find that they need a bit more food than usual. Though we do have nap time during the day, not all dogs will rest. You may wish to let your dog take it easy and have a big snooze after their day of fun.
         </li>
         <li>
-          <span class="font-bold">Thirsty, hungry, tired -</span> As your dog is likely to be much more active at daycare than at home, you may find that they need a bit more food than usual. Though we do have nap time during the day, not all dogs will rest. You may wish to let your dog take it easy and have a big snooze after their day of fun.
+          <span class="font-bold">Illness and Injury -</span> - We have extremely strict hygiene standards and do our utmost to ensure all dogs remain healthy and happy,
+          and we will isolate and inform pawrents immediately if any pup shows any sign of illness. If however, your pup returns ill from
+          daycare, please let us know ASAP. If your dog has a serious injury or displays signs of illness during their time at GDDC we may, at our discretion, take them to a
+          vet, however GDDC does not accept responsibility for any vet bills and as per our daycare agreement you will be liable for any costs involved.
         </li>
         <li>
-          <span class="font-bold">In sickness and in health -</span> GDDC does not accept pups not up to date with their vaccinations, and we will isolate and inform pawrents immediately if any pup shows any sign of illness. We have extremely strict hygiene standards and do our utmost to ensure all dogs remain healthy and happy. If however, your pup returns ill from daycare, please let us know ASAP.
+          <span class="font-bold">Picking Up -</span>  Please note that we close at {{ this.closeTime }}. We kindly ask that you aim to arrive earlier than this if possible, to account for 
+any delays (e.g. traffic) and prevent a rush at closing time. If you are running late, please let us know as early as possible.
         </li>
       </ul>
-      
+
     </div>
     <router-link class="btn-blue inline-block my-5" :to="{ name: 'Sign-up', params: {region: getRegion()} }">Sign me up!</router-link>
   </div>
 </template>
 
 <script>
-export default {
-  computed: {
-    closeTime() {
+  export default {
+    computed: {
+      closeTime() {
         return this.$store.state[this.getRegion()].contact.closeTime
       }
-  } 
-}
+    }
+  }
 </script>
 
 <style>
