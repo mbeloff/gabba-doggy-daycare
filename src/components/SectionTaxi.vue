decb66feb343ed7da23d689ed004ce92dcedbd56
michael beloff <michael.a.beloff@gmail.com>
2021-08-09 12:44:46 +1000

    increase map size

--- src/components/SectionTaxi.vue
+++ src/components/SectionTaxi.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col">
-    <img src="https://res.cloudinary.com/dg5ybbkbh/image/upload/w_600,q_auto,f_auto/v1627338039/gddc/gddc_map.jpg" alt="" class="w-full h-auto">
+    <img src="https://res.cloudinary.com/dg5ybbkbh/image/upload/w_750,q_auto,f_auto/gddc/gddc_map.jpg" alt="" class="w-full h-auto">
     <div class="w-full p-2">
       <div><i class="fas fa-circle text-yellow-400 mr-2"></i><span class="font-bold">Yellow Zone:</span> $10 each way</div>
       <div><i class="fas fa-circle text-red-400 mr-2"></i><span class="font-bold">Red Zone:</span> $20 each way</div>
