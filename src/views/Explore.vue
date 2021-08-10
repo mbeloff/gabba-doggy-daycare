0b92e3c03db6540405261089895cca724554c4a1
michael beloff <michael.a.beloff@gmail.com>
2021-08-10 15:57:14 +1000

    update photos

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -50,11 +50,11 @@ data() {
     areas: {
       brisbane: [
       {
-        heading: 'Huge, indoor daycare',
+        heading: 'Huge, indoor & outdoor daycare',
         desc: "We've created a massive, off-leash dog daycare centre, with multiple spacious play areas and heaps of things to do. Gabba Doggy Daycare is a dog's home away from home.",
         img: [
-          'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616119001/gddc/areas/big.jpg', 
-        'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616119540/gddc/areas/big2.jpg',
+        'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616119001/gddc/areas/big.jpg', 
+        'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1628570195/gddc/photos/2021_August/4.jpg',
         'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616119540/gddc/areas/thing.jpg',
         'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1/gddc/areas/lounge.jpg',
         ],
@@ -62,7 +62,7 @@ data() {
       },
       {
         heading: 'Agility course.',
-        desc: 'Build your dogs confidence with steps, ramps, obstacles and more! Run through, jump over and play upon a wonderful set-up built for fun!',
+        desc: 'Build your dogs confidence with steps, ramps, obstacles and more! Everything built to run through, jump over, and play upon with new friends!',
         img: [
           'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616118677/gddc/areas/agility.jpg',
            'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616118677/gddc/areas/agility2.jpg',
@@ -76,6 +76,7 @@ data() {
         desc: 'It\'s like doggy heaven here with all the balls and toys a pup can handle. Ball pits, chew toys and friends to play with - there\'s never a dull day at GDDC.',
         img: [
           'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616118678/gddc/areas/balls2.jpg', 
+          'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1628571144/gddc/photos/2021_August/5.jpg',
           'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616118677/gddc/areas/balls4.jpg',
           'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616118677/gddc/areas/balls.jpg',
         ],
@@ -83,10 +84,10 @@ data() {
       },
       {
         heading: 'Grass and Sand',
-        desc: 'Dogs love to get some sunshine and play in our large sand pit and grass areas.',
+        desc: 'Dogs love to get some sunshine and exercise in our large grass areas, and sandpits both indoor and outdoor.',
         img: [
           'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616128729/gddc/areas/grass5.jpg', 
-          'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616118680/gddc/areas/grass4.jpg',
+          'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1628571981/gddc/photos/2021_August/7.jpg',    
           'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616118681/gddc/areas/sand2.jpg', 
           'https://res.cloudinary.com/dg5ybbkbh/image/upload/q_auto,f_auto/c_fit,w_300/v1616118680/gddc/areas/sand.jpg'
         ],
