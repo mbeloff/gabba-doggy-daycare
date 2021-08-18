a5813279691cbcedd406fcae3cedaad12555b912
michael beloff <michael.a.beloff@gmail.com>
2021-08-19 09:04:10 +1000

    clarify pet taxi prices

--- src/components/SectionTaxi.vue
+++ src/components/SectionTaxi.vue
@@ -3,8 +3,8 @@
     <img src="https://res.cloudinary.com/dg5ybbkbh/image/upload/w_750,q_auto,f_auto/gddc/gddc_map.jpg" alt="" class="w-full h-auto">
     <div class="w-full p-2">
       <p class="mb-2">Local Pickup and Dropoff</p>
-      <div><i class="fas fa-circle text-yellow-400 mr-2"></i><span class="font-bold">Yellow - Zone One:</span> $20</div>
-      <div><i class="fas fa-circle text-red-400 mr-2"></i><span class="font-bold">Red - Zone Two:</span> $40</div>
+      <div><i class="fas fa-circle text-yellow-400 mr-2"></i><span class="font-bold">Yellow - Zone One:</span> $10 each way</div>
+      <div><i class="fas fa-circle text-red-400 mr-2"></i><span class="font-bold">Red - Zone Two:</span> $15 each way</div>
       <p class="mt-2">Please note: Our pet taxi service has limited availability, and may not always be available. To request a booking, just login to your account and click Request scheduled service.</p>
     </div>
   </div>
