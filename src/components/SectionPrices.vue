f0e99ab80dbe78d4c022bab190f27358909e0414
michael beloff <michael.a.beloff@gmail.com>
2021-08-30 16:03:47 +1000

    adjust pricing info

--- src/components/SectionPrices.vue
+++ src/components/SectionPrices.vue
@@ -6,8 +6,7 @@
           <template #small><span class="capitalize">{{ getRegion() }}</span> Daycare</template>
           <template #big>Price List <br></template>
         </title-block>
-        <p class="-mt-4 text-base text-center tracking-normal font-normal">Open from {{ contact.openTime }} - {{ contact.closeTime }} <br> (Half Day - 5 hours)</p>
-
+        <p class="font-bold text-center mt-5">Casual Day Care</p>
         <div class="grid grid-cols-1 pt-5 gap-x-10 text-sm">
           <div v-for="(service, i) in prices[region].daycare" :key="i" class="mb-1 bg-white" :class="{ 'font-bold' : service.highlight}">
             <div class="flex bg-gray-100 rounded-lg relative ring-pink-200" :class="{ 'ring-2 ' : service.highlight}">
@@ -21,11 +20,8 @@
             </div>
           </div>
         </div>
-        <div class="text-sm mt-4">
-          <p class=" ">The above packages and casual days can be purchased through your PetExec account - <open-login class="link-pink">Login here</open-login></p>
-        </div>
         <p class="font-bold text-center mt-5">Automatic Weekly Payments</p>
-        <p class="text-sm">If you're a regular guest and don't want to worry about buying packages each week, just give us a <a class="link-pink" :href="`tel:` + removeSpaces(contact.phone)">call</a> or <link-email class='link-pink'>email</link-email> and we can set up a discounted recurring weekly payment for you. These payments can be paused or cancelled at any time :)</p>
+        <p class="text-sm pt-5">Set up recurring billing to get discounted Day Care and now earn <span class="link-pink">paw points</span> which can be redeemed for extras such as dog washes. One automatic payment each Sunday means all you have to worry about is dropping off and picking up your pup - and of course you can pause or adjust your payments if your schedule changes, just let us know before your usual billing day.</p>
         <div class="grid grid-cols-1 pt-5 gap-x-10 text-sm">
           <div v-for="(service, i) in prices[region].directdebit" :key="i" class="mb-1 bg-white" :class="{ 'font-bold' : service.highlight}">
             <div class="flex bg-gray-100 rounded-lg relative ring-pink-200" :class="{ 'ring-2 ' : service.highlight}">
@@ -40,10 +36,8 @@
           </div>
         </div>
         <div id="extras-pricing" v-if="prices[region].extras">
-          <title-block class="text-center">
-            <template #big>Extras</template>
-          </title-block>
-          <div class="grid grid-cols-1 pt-5 gap-x-10 text-sm">
+          <p class="font-bold text-center mt-5">Extras</p>
+          <div class="grid grid-cols-1 pt-5 gap-x-10 text-sm ">
             <div v-for="(extra, i) in prices[region].extras" :key="i" class="mb-2 bg-white">
               <div @click="showTaxi" class="flex bg-gray-100 rounded-lg" :class="{ 'taxi cursor-pointer' : extra.selector == 'taxi' }">
                 <div class="flex-grow px-2" :class="{ 'opacity-50' : extra.soon == '- available soon'}">{{extra.name}} <span class="text-pink-500">{{ extra.soon }}</span></div>
@@ -57,17 +51,18 @@
       </div>
     </div>
     <div class="mt-10 px-5 text-sm gap-2 flex flex-col">
-      <p>Package Terms and Conditions: </p>
-      <p>12-month unlimited package is only available to desexed dogs, and may be subject to approval.</p> 
+      <p class="font-bold">Package Terms and Conditions: </p>
       <p>
-        Please note that our unlimited daycare packages are valid for all days that we are open, which may not necessarily be 5 days every week. Any days you opt not to use, or days we may be closed, including public holidays, may not be refunded or carried over to following weeks.        
+        12-month unlimited package is only available to desexed dogs over 6 months, and may be subject to approval.</p> 
+      <p>
+        Unlimited daycare packages are valid for all days that we are open, which may not necessarily be 5 days every week. Any days you opt not to use, or days we may be closed, including public holidays, may not be refunded or carried over to following weeks.
       </p>
       <p>
-        If we are fully booked, guests who have pre-paid will be given priority placement over casual visitors. Unlimited daycare packages may become temporarily unavailable to new sign-ups if we are approaching full capacity.
-      </p> 
-      <p>
-        If you would like to cancel, pause, or adjust your weekly payment to a different package, just let us know before your usual billing day and we'll be happy to accommodate <i class="fal fa-smile"></i>
+        If you would like to cancel, pause, or adjust your weekly payment to a different package, just let us know before your usual billing day and we'll be happy to accommodate <i class="fal fa-smile"></i> 
       </p>
+      <p>
+        If we are fully booked, customers on recurring billing will be given priority placement. 
+      </p>       
     </div>
   </div>
 </template>
@@ -97,10 +92,12 @@
             daycare: {
               HalfDay: {
                 name: 'Half Day',
+                soon: " - up to 5 hours",
                 price: "$40",
               },
               FullDay: {
                 name: 'Full Day',
+                soon: " - over 5 hours",
                 price: "$50",
               },
               HalfDayPack: {
@@ -122,19 +119,23 @@
             directdebit: {
               OneDay: {
                 name: "One Day a Week",
-                price: "$50",
+                price: "$48",
               },
               TwoDays: {
                 name: "Two Days a Week",
-                price: "$96 ($48/day)",
+                price: "$92 ($46/day)",
               },
               ThreeDays: {
                 name: "Three Days a Week",
-                price: "$138 ($46/day)",
+                price: "$132 ($44/day)",
+              },
+              FourDays: {
+                name: "Four Days a Week",
+                price: "$168 ($42/day)",
               },
               FullDayWeek: {
                 name: "Unlimited Daycare",
-                price: "$150 (~$30/day)",
+                price: "$175 (~$35/day)",
                 highlight: true,
               },
             },
@@ -147,10 +148,10 @@
               },
               PetTaxi: {
                 selector: 'taxi',
-                name: "Pet Taxi (Local Pickup and Dropoff)",
+                name: "Pet Taxi (Local Pickup or Dropoff)",
                 // desc: "Local pickup & dropoff in our Pet Taxi (within 5km)",
                 soon: "- more info",
-                price: "$20-40",
+                price: "$10-15",
                 order: '3'
               },
             }
